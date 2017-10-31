088717b61df7909f1ba1cceb806801779f2909de
ccl <528050239@qq.com>
2017-10-31 14:18:21 +0800

    Added BitcoinConst

---
+++ BackEnd/themis-common/src/main/java/com/oxchains/themis/common/bitcoin/BitcoinConst.java
@@ -0,0 +1,11 @@
+package com.oxchains.themis.common.bitcoin;
+
+/**
+ * @author ccl
+ * @time 2017-10-31 14:10
+ * @name BitcoinConst
+ * @desc:
+ */
+public class BitcoinConst {
+    private BitcoinConst(){}
+}
