9ff3694cc9197e5ac45f631166f38bd14e2fba04
ccl <528050239@qq.com>
2017-11-09 16:58:41 +0800

    Added module themis-zuul

---
+++ BackEnd/themis-zuul/src/main/java/com/oxchains/themis/ZuulApplication.java
@@ -0,0 +1,21 @@
+package com.oxchains.themis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+/**
+ * @author ccl
+ * @time 2017-11-09 16:45
+ * @name ZuulApplication
+ * @desc:
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+}
