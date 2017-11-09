6db990d74b2839595ed4dd95f447d66903f49317
ccl <528050239@qq.com>
2017-11-09 15:09:40 +0800

    Modified versions:mvn versions:set -DnewVersion=1.0.1-SNAPSHOT

--- BackEnd/bitcoin-rpc-client/src/main/java/com/oxchains/bitcoin/rpcclient/BitcoinJSONRPCClient.java
+++ BackEnd/bitcoin-rpc-client/src/main/java/com/oxchains/bitcoin/rpcclient/BitcoinJSONRPCClient.java
@@ -4,6 +4,8 @@ import com.oxchains.bitcoin.util.BitcoinUtil;
 
 import java.math.BigDecimal;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 
 /**
@@ -13,6 +15,21 @@ import java.util.*;
  * @desc:
  */
 public class BitcoinJSONRPCClient extends BaseBitcoinJSONRPCClient {
+
+    public BitcoinJSONRPCClient() {
+    }
+
+    public BitcoinJSONRPCClient(URL rpc) {
+        super(rpc);
+    }
+
+    public BitcoinJSONRPCClient(String rpcUrl) throws MalformedURLException {
+        super(rpcUrl);
+    }
+
+    public BitcoinJSONRPCClient(boolean testNet) {
+        super(testNet);
+    }
 
     /** <> *** bitcoin rpcs ***</> */
 
