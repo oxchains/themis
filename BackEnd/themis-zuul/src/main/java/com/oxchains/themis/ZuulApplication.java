e3c236cd7d1bba6ec05d4af61feb756caf5a4a4b
ccl <528050239@qq.com>
2017-11-10 11:45:52 +0800

    Updated configurations

--- BackEnd/themis-zuul/src/main/java/com/oxchains/themis/ZuulApplication.java
+++ BackEnd/themis-zuul/src/main/java/com/oxchains/themis/ZuulApplication.java
@@ -1,9 +1,12 @@
 package com.oxchains.themis;
 
+import com.oxchains.themis.zuul.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
 /**
  * @author ccl
  * @time 2017-11-09 16:45
@@ -17,5 +20,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class,args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 }
